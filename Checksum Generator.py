b967b81b6a5412c4660ed3239acd67e440ec031c
MateuszMrooz <20mmroz.acc@lwetb.ie>
2024-11-12 13:24:23 +0000

    Add files via upload

---
+++ Checksum Generator.py
@@ -0,0 +1,18 @@
+num = int(input('Enter a 9 digit number '))
+count = 1
+total = 0
+finalnum = num
+while count <= 9:
+	count = count + 1
+	num1 = num%10
+	num = num//10
+	total = total + num1*count
+num2 = 11
+while num2 < total:
+	num2 = num2 + 11
+checksum = num2 - total
+if checksum == 10:
+	checksum = X
+print(finalnum,checksum,sep='')
+
+#020131452
